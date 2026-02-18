7fe93b6002164465b4b0a4d140f892b4961b81ac
YASHU <yashvardhanchoudhary6@gmail.com>
2026-02-18 22:01:45 +0530

    Add files via upload

---
+++ day18.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i;
+
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &k);
+
+    k = k % n;   // Handle cases where k > n
+
+    // Print rotated array
+    for(i = n - k; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    for(i = 0; i < n - k; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
