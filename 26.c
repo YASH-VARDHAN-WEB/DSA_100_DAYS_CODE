5d0b1af90c5c07e5a9aaa40490533e8377e98535
YASHU <yashvardhanchoudhary6@gmail.com>
2026-02-26 09:10:02 +0530

    Add files via upload

---
+++ 26.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+int main() {
+    int n, value;
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Input number of nodes
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &value);
+
+        // Create new node
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+        newNode->prev = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            newNode->prev = temp;
+            temp = newNode;
+        }
+    }
+
+    // Forward Traversal
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
